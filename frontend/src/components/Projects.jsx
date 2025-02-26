25d0ca8311b2a0dac5ebf2ca5db0c146cb0f31ef
Sandip Singh Parmar <sandipsinghparmar18@gmail.com>
2025-02-26 23:09:24 +0530

    add project

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import rentConnectImg from '../assets/rentconnect.png'; // Make sure to replace with the correct path to your image
+import videotubeImage from '../assets/videotube.webp';
 
 function Projects() {
   return (
@@ -7,7 +8,7 @@ function Projects() {
       <h2 className="text-3xl font-bold text-gray-800 text-center">Projects</h2>
       <div className="mt-8 flex flex-wrap justify-center">
         <a
-          href="https://your-project-link.com" // Replace with your project link
+          href="https://videotubedist.onrender.com" // Replace with your project link
           target="_blank"
           rel="noopener noreferrer"
           className="transform transition-transform hover:scale-105"
@@ -16,14 +17,16 @@ function Projects() {
         >
           <div className="bg-white rounded-lg shadow-md max-w-md p-6 m-4 hover:bg-gradient-to-r hover:from-teal-400 hover:to-blue-500 hover:text-white hover:shadow-xl transition duration-300">
             <img
-              src={rentConnectImg}
+              src={videotubeImage}
               alt="RentConnect Project"
               className="w-full rounded-lg mb-4"
             />
-            <h3 className="text-xl font-bold text-gray-800 hover:text-white">RentConnect</h3>
+            <h3 className="text-xl font-bold text-gray-800 hover:text-white">VideoTube</h3>
             <p className="text-gray-600 mt-2 hover:text-white">
-              A web application inspired by Airbnb, connecting property owners and renters. Built with
-              JavaScript, EJS, Express, Node.js, and MongoDB.
+            VideoTube is a video-sharing platform built with React (Vite), Tailwind CSS, Node.js,
+             and Express, featuring video uploads, likes, comments, subscriptions, and authentication. 
+             It integrates Cloudinary for media storage, offering a seamless user experience with a 
+             YouTube-inspired UI. 🚀.
             </p>
           </div>
         </a>
